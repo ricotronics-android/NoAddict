7534747178f82770f6ca6a8bf2e104c78b7dcb8c
ocirnexam <kontakt.maxmueller@outlook.com>
2025-04-27 12:11:53 +0200

    Fixed implicit usage of locale

--- app/src/main/java/com/ricotronics/noaddict/utils/formatTime.kt
+++ app/src/main/java/com/ricotronics/noaddict/utils/formatTime.kt
@@ -1,9 +1,11 @@
 package com.ricotronics.noaddict.utils
 
+import java.util.Locale
+
 fun Long.formatTime(): String {
     val days = (this / 3600) / 24
     val hours = (this / 3600) % 24
     val minutes = (this % 3600) / 60
     val seconds = this % 60
-    return String.format("%02d Days\n%02d Hours\n%02d Minutes\n%02d Seconds", days, hours, minutes, seconds)
+    return String.format(Locale.GERMAN,"%02d Days\n%02d Hours\n%02d Minutes\n%02d Seconds", days, hours, minutes, seconds)
 }
